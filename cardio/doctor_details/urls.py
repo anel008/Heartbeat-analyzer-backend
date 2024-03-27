7e95c5c65de51847f3cd6a0046ffdcc2f423c451
anel babu <anelbabu008@gmail.com>
2024-03-28 00:39:55 +0530

    urls arranged

--- cardio/doctor_details/urls.py
+++ cardio/doctor_details/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path
 from .views import d_create,d_details,Update_Doctor,Delete_Doctor #,p_create #,DoctorUpdateDelete,Search_Doctors
-from patient_details.views import p_create,Search_Patient,p_details
+from patient_details.views import p_create,Search_Patient,p_details,Update_Patient,Delete_patient
 
 
 
 urlpatterns = [
     
+    #***********DOCTOR**********#
+    
     path('dc',d_create.as_view()),#doctor create
     path('dd',d_details.as_view()),#doctor details
-    path('pd',p_details.as_view()),#patient detail view
-    path('pc',p_create.as_view()),   #patient create
     path('Dupdate/<int:license_no>',Update_Doctor.as_view()),   # update doctor
     path('Ddelete/<str:license_no>',Delete_Doctor.as_view()),   # Delete doctor
+    
+
+    #**********PATIENT IN DOCTOR **********#
+
+    path('pd',p_details.as_view()),#patient detail view
+    path('pc',p_create.as_view()),   #patient create
+    path('Pupdate/<str:name>',Update_Patient.as_view()),   # update the patient details
+    path('Pdelete/<str:name>',Delete_patient.as_view()),   # delete patient
     path('Psearch',Search_Patient.as_view({'get':'list'})) # patient search 
 
 ]
